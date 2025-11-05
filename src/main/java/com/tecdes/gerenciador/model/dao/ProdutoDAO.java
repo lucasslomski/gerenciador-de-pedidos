e029c1d25bc313398c630e5c6eebe0c8b21d94a4
lucasslomski <lucas_slomski@estudante.sesisenai.org.br>
2025-11-04 21:48:19 -0300

    Solve DAO
    Resolvido o entity passado o código vigente no DAO

--- src/main/java/com/tecdes/gerenciador/model/dao/ProdutoDAO.java
+++ src/main/java/com/tecdes/gerenciador/model/dao/ProdutoDAO.java
@@ -1,38 +1,5 @@
 package com.tecdes.gerenciador.model.dao;
 
 public class ProdutoDAO {
-    private int codProduto;
-    private int nomeProduto;
-    private double precoProduto;
 
-
-    public ProdutoDAO(){}
-
-    //Construtor
-    public ProdutoDAO(int codProduto, int nomeProduto, double precoProduto) {
-        this.codProduto = codProduto;
-        this.nomeProduto = nomeProduto;
-        this.precoProduto = precoProduto;
-    }
-
-    //Getters e Setters
-
-    public int getCodProduto() {
-        return codProduto;
-    }
-    public void setCodProduto(int codProduto) {
-        this.codProduto = codProduto;
-    }
-    public int getNomeProduto() {
-        return nomeProduto;
-    }
-    public void setNomeProduto(int nomeProduto) {
-        this.nomeProduto = nomeProduto;
-    }
-    public double getPrecoProduto() {
-        return precoProduto;
-    }
-    public void setPrecoProduto(double precoProduto) {
-        this.precoProduto = precoProduto;
-    }
 }
---
+++ src/main/java/com/tecdes/gerenciador/model/dao/entity/Produto.java
@@ -0,0 +1,5 @@
+package com.tecdes.gerenciador.model.dao.entity;
+
+public class Produto {
+
+}
