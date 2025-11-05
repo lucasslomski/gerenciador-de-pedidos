537ae36b8eefe15b35419ce45f21442a5d3f3c77
Caíque Oliveira <caique_oliveira@estudante.sc.senai.br>
2025-11-04 21:45:01 -0300

    ImplantacaoEntity
    Inicialização de implantação do Produto (Entity)

--- src/main/java/com/tecdes/gerenciador/model/entity/Produto.java
+++ src/main/java/com/tecdes/gerenciador/model/entity/Produto.java
@@ -1,5 +1,15 @@
-package com.tecdes.gerenciador.entity;
+package com.tecdes.gerenciador.model.entity;
 
 public class Produto {
+    private int id_produto;
+    private int cd_produto;
+    private String nm_produto;
+    private double nr_preco_produto;
+
+    public Produto(){};
+
+    public Produto(String nm_produto, int cd_produto){
+        
+    }
 
 }
